9b8d836811cd3c002bc0963a0b802c59f6902091
cryptocatdevelopment <cryptocatdevelopment@gmail.com>
2022-05-02 20:47:42 +0200

    comit

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias architecto numquam molestiae, atque esse nesciunt obcaecati. Quos ducimus ex ratione, eaque aspernatur deserunt, recusandae autem, dicta repudiandae suscipit obcaecati neque.
+    <?php echo $msg ?>
+</body>
+</html>
